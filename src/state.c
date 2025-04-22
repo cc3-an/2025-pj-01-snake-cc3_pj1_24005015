4150bd1d131794d64c3f1e48b6bf4c6a8b6fbc5d
CC3 <24005015@galileo.edu>
2025-04-22 17:33:22 -0600

    correcion

--- src/state.c
+++ src/state.c
@@ -284,22 +284,27 @@ static void update_head(game_state_t* state, unsigned int snum) {
 static void update_tail(game_state_t* state, unsigned int snum) {
   // TODO: Implementar esta funcion.
   snake_t *snake = &state->snakes[snum];
-
   if (snake->live) {
     unsigned int tail_row = snake->tail_row;
     unsigned int tail_col = snake->tail_col;
 
     set_board_at(state, tail_row, tail_col, ' ');
 
-    unsigned int new_tail_row = snake->tail_row;
-    unsigned int new_tail_col = snake->tail_col;
+    if (snake->length > 1) {
+      unsigned int new_tail_row = snake->body[snake->length - 2].row;
+      unsigned int new_tail_col = snake->body[snake->length - 2].col;
 
-    snake->tail_row = new_tail_row;
-    snake->tail_col = new_tail_col;
+      snake->tail_row = new_tail_row;
+      snake->tail_col = new_tail_col;
 
-    char tail_char = 'w';
-    set_board_at(state, snake->tail_row, snake->tail_col, tail_char);
+      char tail_char = 'w';
+      set_board_at(state, snake->tail_row, snake->tail_col, tail_char);
+    } else {
+      
+
+    }
   }
+
 }
 
 /* Tarea 4.5 */
