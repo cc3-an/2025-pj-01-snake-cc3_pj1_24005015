4d56f1df60aa23af7620eb18e12798f93249f847
CC3 <24005015@galileo.edu>
2025-04-22 23:10:45 -0600

    corregido

--- src/state.c
+++ src/state.c
@@ -355,7 +355,7 @@ void update_state(game_state_t* state, int (*add_food)(game_state_t* state)) {
 /* Tarea 5 */
 game_state_t* load_board(char* filename) {
   // TODO: Implementar esta funcion.
-    FILE* fp = &fopen(filename, "r");
+    FILE* fp = fopen(filename, "r");
     if (fp == NULL) {
         perror("Error al abrir archivo");
         return NULL;
@@ -364,13 +364,12 @@ game_state_t* load_board(char* filename) {
     game_state_t* state = malloc(sizeof(game_state_t));
     state->num_rows = 0;
     state->board = NULL;
-    state->row_lengths = NULL;
-    state->num_snakes = 0;
     state->snakes = NULL;
+    state->num_snakes = 0;
 
-    char buffer[MAX_LINE_LENGTH];
+    char buffer[1024]; 
 
-    while (fgets(buffer, MAX_LINE_LENGTH, f)) {
+    while (fgets(buffer, sizeof(buffer), fp)) {
         size_t len = strlen(buffer);
 
         if (len > 0 && buffer[len - 1] == '\n') {
@@ -379,14 +378,8 @@ game_state_t* load_board(char* filename) {
         }
 
         state->board = realloc(state->board, sizeof(char*) * (state->num_rows + 1));
-        state->row_lengths = realloc(state->row_lengths, sizeof(unsigned int) * (state->num_rows + 1));
-
-        char* row = malloc(sizeof(char) * (len + 1)); 
-        strcpy(row, buffer);
-
-        state->board[state->num_rows] = row;
-        state->row_lengths[state->num_rows] = (unsigned int)len;
-        state->num_rows++;
+        state->board[state->num_rows] = strdup(buffer);
+        state->num_rows++
     }
 
     fclose(f);
