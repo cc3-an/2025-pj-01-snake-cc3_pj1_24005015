2343a4044a99f2c89619cfdccd46527be0d87e5f
CC3 <24005015@galileo.edu>
2025-05-04 19:42:32 -0600

    correccion

--- src/state.c
+++ src/state.c
@@ -434,6 +434,7 @@ game_state_t* load_board(char* filename) {
 */
 static void find_head(game_state_t* state, unsigned int snum) {
   // TODO: Implementar esta funcion.
+    snake_t* snake = &state->snakes[snum];
     unsigned int row = snake->tail_row;
     unsigned int col = snake->tail_col;
     char c = state->board[row][col];
@@ -443,17 +444,8 @@ static void find_head(game_state_t* state, unsigned int snum) {
         col = get_next_col(col, c);
         c = state->board[row][col];
 
-        if (c == '>' || c == '<' || c == '^' || c == 'v' ||
-            c == 'd' || c == 'a' || c == 'w' || c == 's') {
-
-            unsigned int next_row = get_next_row(row, c);
-            unsigned int next_col = get_next_col(col, c);
-            char next_c = state->board[next_row][next_col];
-
-            if (next_c != '#' && next_c != '-' && next_c != '|' &&
-                next_c != '<' && next_c != '>' && next_c != '^' && next_c != 'v') {
-                break;
-            }
+        if (c == 'W' || c == 'A' || c == 'S' || c == 'D') {
+            break;
         }
     }
 
